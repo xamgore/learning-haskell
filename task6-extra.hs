0b725dc4aba7e40748e5f8ce6a3bc4ce5f96a9d7
xamgore <xamgore@ya.ru>
2016-09-29 02:23:55 +0300

    Implement some functions by type annotations

--- task6-extra.hs
+++ task6-extra.hs
@@ -10,9 +10,23 @@ get xs n = last $ take n xs
 
 -- 3) [a] -> Int -> [a]
 
+-- skip first n elements
+drop'                       :: [a] -> Int -> [a]
+drop' xs     n     | n <= 0 = xs
+drop' []     _              = []
+drop' (_:xs) n              = drop' xs (n-1)
+
+-- skip everything except first n elements
+take'                       :: [a] -> Int -> [a]
+take' _      n     | n <= 0 = []
+take' []     _              = []
+take' (x:xs) n              = x : take' xs (n-1)
 
 -- 4) a -> Int -> [a]
 
+replicate'              :: a -> Int -> [a]
+replicate' _ n | n <= 0 = []
+replicate' x n          = x : replicate' x (n-1)
 
 -- 5) [a] -> [a] -> [a]
 
