4ef9c2ffe87a9c682ba44211739fa0a409241fc2
xamgore <xamgore@ya.ru>
2016-10-14 13:29:54 +0300

    Complete task 1 (nub)

--- 01-nub.hs
+++ 01-nub.hs
@@ -1,37 +1,50 @@
+{-# LANGUAGE ViewPatterns #-}
+
 {-
   Дан текстовый файл numbers.txt, содержащий целые числа
   в диапазоне от 1 до 1000, разделённые пробелами и символами перевода строки. Определить
   количество различных чисел в нём, пользуясь для этого возможностями различных структур
-  данных. 
+  данных.
 -}
 
-import Data.List
-import qualified Data.Sequence as Seq
+import Data.List (nub)
+import Data.Sequence (ViewL( (:<) ), (<|) )
+import qualified Data.Sequence as S
 import qualified Data.IntSet as Set
 import Data.Array.IArray
-import System.Environment
 import Control.Monad
 
-nub_set :: Set.IntSet -> Int
-nub_set = Set.size
+nubSet :: Set.IntSet -> Int
+nubSet = Set.size
 
-nub_list :: [Int] -> Int
-nub_list = undefined
+nubList :: [Int] -> Int
+nubList = length . nub
 
-nub_seq :: Seq.Seq a -> Int
-nub_seq = undefined
+group :: Eq a => S.Seq a -> S.Seq (S.Seq a)
+group (S.viewl -> x :< xs) = (x <| ys) <| group zs
+    where (ys, zs) = S.spanl (x ==) xs
+group _ = S.empty
 
-nub_arr :: Array Int Int -> Int
-nub_arr = undefined
+nubSeq :: Ord a => S.Seq a -> Int
+nubSeq = S.length . group . S.sort
 
+nubArr :: Array Int Int -> Int
+nubArr arr = sum (accumArray max 0 (1, 1000) elements :: Array Int Int)
+    where elements = [(arr!i, 1) | i <- range (bounds arr)]
+
+
+main :: IO ()
 main = do
-  let fname = "numbers.txt"
-  content <- readFile fname
-  let xs = map read $ concatMap words $ lines content
-      n = nub_set $ Set.fromList xs
-      results = [
-        nub_list xs,
-        nub_seq $ Seq.fromList xs,
-        nub_arr $ listArray (1,length xs) xs ]
-  mapM_ print results
-  when (any (/= n) results) $ putStrLn "Результаты не совпадают!"
+    let fname = "numbers.txt"
+    content <- readFile fname
+
+    let xs = map read $ concatMap words $ lines content
+        n  = nubSet $ Set.fromList xs
+
+        results = [
+            nubList xs,
+            nubSeq $ S.fromList xs,
+            nubArr $ listArray (1, length xs) xs ]
+
+    mapM_ print results
+    when (any (/= n) results) $ putStrLn "Результаты не совпадают!"
