b0ccabe72c251ce604f9411315ac1c10b6f3b67a
xamgore <xamgore@ya.ru>
2016-11-25 12:54:43 +0300

    Implement bubble sort

--- 01-ST.hs
+++ 01-ST.hs
@@ -2,23 +2,37 @@
 
 import Data.STRef
 import Control.Monad.ST
+import Control.Monad
 
 import Data.Array
 import Data.Array.ST
 import Data.Array.MArray
 
-swapElems :: Ix i => i -> i -> STArray s i e -> ST s ()
-swapElems i j arr = do
-     vi <- readArray arr i
-     vj <- readArray arr j
-     writeArray arr i vj
-     writeArray arr j vi
+moveUp :: (Ord e, Ix i, Integral i) => i -> i -> STArray s i e -> ST s ()
+moveUp i j arr | i < j = do
+     x <- readArray arr i
+     y <- readArray arr j
 
-test :: Int -> Int -> [a] -> [a]
-test i j xs = elems $ runSTArray $ do
-    arr <- newListArray (0, length xs - 1) xs
-    swapElems i j arr
+     when (x > y) $ do
+         writeArray arr i y
+         writeArray arr j x
+
+
+bubbleST :: (Ord e, Ix i, Integral i) => STArray s i e -> ST s (STArray s i e)
+bubbleST arr = do
+    (li, ri) <- getBounds arr
+
+    forM_ [li .. ri] $ \_ ->
+        forM_ [li .. ri - 1] $ \j ->
+            moveUp j (j + 1) arr
+
     return arr
 
-mysort :: [a] -> [a]
-mysort = undefined
+
+sort :: (Ord a) => [a] -> [a]
+sort xs = elems $ runSTArray $ do
+    arr <- newListArray (0, length xs - 1) xs
+    bubbleST arr
+    return arr
+
+test_sort = sort [5, 1, 3, 4, 2] == [1..5]
