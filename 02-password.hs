3871853d24b5c75e450de66cbae4863e2719f30e
xamgore <xamgore@ya.ru>
2016-12-07 22:02:07 +0300

    Implement task 2

--- 02-password.hs
+++ 02-password.hs
@@ -11,25 +11,40 @@
 import Control.Monad
 import Control.Monad.Trans
 import Control.Monad.Trans.Maybe
+import Control.Monad.Trans.Reader
+import Control.Monad.Trans.Writer
+import System.Environment
 
 import Data.Char
 
-isValid :: String -> Bool
-isValid s = length s >= 8 && 
-                any isAlpha s && 
-                any isNumber s && 
-                any isPunctuation s
+data Config = Config Int Bool Bool Bool
 
-getValidPassword :: MaybeT IO String
-getValidPassword = do
-  lift $ putStrLn "Введите новый пароль:"
-  s <- lift getLine
-  guard (isValid s)
-  return s
- 
-askPassword :: MaybeT IO ()
+main = do
+    [minLen, alpha, numbers, other] <- getArgs
+    let cfg = Config (read minLen) (read alpha) (read numbers) (read other)
+    story <- runWriterT $ runReaderT (runMaybeT askPassword) cfg
+    print $ snd story
+
+
+askPassword :: MaybeT (ReaderT Config (WriterT [String] IO)) ()
 askPassword = do
-  value <- msum $ repeat getValidPassword
-  lift $ putStrLn "Сохранение в базе данных..."
+    value <- msum $ repeat getValidPassword
+    liftIO $ putStrLn "Сохранение в базе данных..."
 
-main = runMaybeT askPassword
+getValidPassword :: MaybeT (ReaderT Config (WriterT [String] IO)) String
+getValidPassword = do
+  liftIO $ putStrLn "Введите новый пароль:"
+  passw <- liftIO getLine
+  lift $ lift $ tell [passw]
+  valid <- lift $ isValid passw
+  guard valid
+  return passw
+
+isValid :: String -> (ReaderT Config (WriterT [String] IO)) Bool
+isValid s = do
+    (Config minLen alpha numbers other) <- ask
+    return $ and [
+        length s >= minLen,
+        not alpha   || any isAlpha  s,
+        not numbers || any isNumber s,
+        not other   || any isPunctuation s ]
